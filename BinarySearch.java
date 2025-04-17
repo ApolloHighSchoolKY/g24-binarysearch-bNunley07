5c991e9f95fa0d42b1b32a0121384384c8cbf52b
bNunley07 <benjamin.nunley@stu.daviess.kyschools.us>
2025-04-17 02:32:03 +0000

    ssssss

--- BinarySearch.java
+++ BinarySearch.java
@@ -4,6 +4,32 @@ public class BinarySearch
 {
 	public static int binarySearch(int[] ray, int item)
 	{
+		Arrays.sort(ray);
+		int bottom = 0;
+		int top = ray.length-1;
+		int middle = 0;
+		while(bottom <= top)
+		{
+		middle = (bottom + top)/2;
+		if(ray[middle] > item)
+		{
+			top = middle-1;
+		}
+		else if(ray[middle] < item)
+		{
+			bottom = middle+1;
+		}
+		else
+		{
+			return middle;
+		}
+		}
+		if(ray[middle] == item)
+		return middle;
 
+		else{
+			return -1;
+		}
 	}
+	
 }
